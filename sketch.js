4dcdda373127f13515ec50535c3693e9ab25a214
priyankasaxena309 <78557238+priyankasaxena309@users.noreply.github.com>
2022-06-22 19:22:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var bg, bgImg
+var bottomGround
+var topGround
+var balloon, balloonImg
+
+function preload(){
+bgImg = loadImage("assets/bg.png")
+
+balloonImg = loadAnimation("assets/balloon1.png","assets/balloon2.png","assets/balloon3.png")
+}
+
+function setup(){
+
+
+}
+
+function draw() {
+  
+
+        
+}
